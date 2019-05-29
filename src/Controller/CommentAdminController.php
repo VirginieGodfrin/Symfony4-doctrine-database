d26957359748357d2656fe34d4cd9638a7742217
VirginieGodfrin <virginiegodfrin@gmail.com>
2019-05-29 10:54:37 +0200

    Chap12: The Twig Extensions Library

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Repository\CommentRepository;
 
 class CommentAdminController extends Controller
 {
     /**
      * @Route("/admin/comment", name="comment_admin")
      */
-    public function index()
+    public function index(CommentRepository $repository)
     {
+    	$comments = $repository->findBy([], ['createdAt' => 'DESC']);
         return $this->render('comment_admin/index.html.twig', [
-            'controller_name' => 'CommentAdminController',
+            'comments' => $comments,
         ]);
     }
 }
